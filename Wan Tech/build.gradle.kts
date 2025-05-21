f32d25278ee99c9b94c1e77c24b9e9899d410a23
Miguel Angel Tapiero Puentes <heglet@gmail.com>
2025-05-20 21:28:17 -0500

    Ajuste build.gradle.kts

--- Wan Tech/build.gradle.kts
+++ Wan Tech/build.gradle.kts
@@ -62,8 +62,8 @@ dependencies {
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation 'com.google.android.gms:play-services-maps:18.2.0'
-    implementation 'com.google.android.gms:play-services-location:21.0.1'
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
 
     // Google Play Services - Location
     implementation("com.google.android.gms:play-services-location:21.2.0")
